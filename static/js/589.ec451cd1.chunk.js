7a539dd8b2d44aec5792d1d74124390a37a4a213
Gaondarech726 <123805187+Gaondarech726@users.noreply.github.com>
2025-02-14 19:34:13 +0000

    Deploying to gh-pages from @ Gaondarech726/siteM8@84acb50580cf949a1a0b55e6100b3ca17e3656c1 🚀

---
+++ static/js/589.ec451cd1.chunk.js
@@ -0,0 +1,37 @@
+"use strict";(self.webpackChunksiteM8=self.webpackChunksiteM8||[]).push([[589],{2589:(e,i,r)=>{r.r(i),r.d(i,{CardWrapper:()=>c,Container:()=>s,default:()=>l});var o=r(8035),t=r(5867),n=r(184);const s=t.ZP.div`
+  width: 928px;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  gap: 16px;
+`,c=t.ZP.div`
+  border: 1px solid white;
+  width: 928px;
+  height: 64px;
+  border-radius: 4px;
+  transition: background-color 0.5s ease, color 0.5s ease;
+  color: white;
+
+  &:hover {
+    background-color: white;
+    color: black;
+  }
+
+  > a {
+    font-size: 18px;
+    font-weight: 600;
+    text-decoration: none;
+    display: block;
+    padding: 20px;
+    color: inherit;
+    transition: color 0.5s ease;
+  }
+  h3 {
+    color: inherit;
+    height: 32px;
+    width: 928px;
+    display: flex;
+    align-items: center;
+  }
+`,l=()=>(0,n.jsxs)(s,{children:[(0,n.jsx)(c,{children:(0,n.jsx)(o.rU,{to:"manifest",children:"\u041c\u0430\u043d\u0456\u0444\u0435\u0441\u0442"})}),(0,n.jsx)(c,{children:(0,n.jsx)(o.rU,{to:"runes",children:"\u041f\u0440\u043e \u0440\u0443\u043d\u0438"})}),(0,n.jsx)(c,{children:(0,n.jsx)(o.rU,{to:"concert",children:"\u041a\u043e\u043d\u0446\u0435\u0440\u0442 M8L8TH"})}),(0,n.jsx)(c,{children:(0,n.jsx)(o.rU,{to:"sxe",children:"\u0411\u0443\u0434\u044c \u0442\u0432\u0435\u0440\u0435\u0437\u0438\u043c"})})]})}}]);
+//# sourceMappingURL=589.ec451cd1.chunk.js.map
