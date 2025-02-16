a5764620fd9797b18e4239ec5f1d0ab83bc64e96
Anton Honcharenko <123805187+Gaondarech726@users.noreply.github.com>
2025-02-16 18:09:08 +0200

    add author

--- src/components/ProductList.styled.js
+++ src/components/ProductList.styled.js
@@ -5,12 +5,13 @@ export const Container = styled.div`
   flex-direction: column;
   display: flex;
   justify-content: center;
-  gap: 16px;
+  gap: 25px;
 `;
 
 export const CardWrapper = styled.div`
+  border: 1px solid white;
   width: 380px;
-  height: 64px;
+  height: 70px;
   border-radius: 4px;
   transition: background-color 0.5s ease, color 0.5s ease;
   color: white;
@@ -38,6 +39,12 @@ export const CardWrapper = styled.div`
 
 export const ProductName = styled.h3`
   color: white;
+`;
+
+export const ProductAuthor = styled.span`
+  color: #bd1e2c;
+  font-size: 12px;
+  font-weight: 600;
 `;
 
 export const ProductH1 = styled.h3`
