7147da764893a29dfb72be131fc3fca9ef3f0905
Gaondarech726 <123805187+Gaondarech726@users.noreply.github.com>
2025-02-18 07:11:34 +0000

    Deploying to gh-pages from @ Gaondarech726/siteM8@43db32ee86ec73ab96f7c8ea790b1c01f2dd270b 🚀

---
+++ static/js/358.9637fb7e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksiteM8=self.webpackChunksiteM8||[]).push([[358],{358:(e,a,l)=>{l.d(a,{jTe:()=>i});var t=l(2119);function i(e){return(0,t.w_)({tag:"svg",attr:{viewBox:"0 0 20 20",fill:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z",clipRule:"evenodd"},child:[]}]})(e)}}}]);
+//# sourceMappingURL=358.9637fb7e.chunk.js.map
