8410be8ba8d0bfc6e597cf797049f60e7445956f
Anton Honcharenko <123805187+Gaondarech726@users.noreply.github.com>
2025-03-03 20:44:43 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import 'modern-normalize';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './components/App';
 import './index.css';
 import './pages/feauters/loading/load.scss';
-
-
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter basename="/siteM8">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
