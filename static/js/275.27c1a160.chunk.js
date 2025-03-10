ce10abbae27d9a11cd00afa82819d7bde6acc940
Gaondarech726 <123805187+Gaondarech726@users.noreply.github.com>
2025-03-10 08:45:23 +0000

    Deploying to gh-pages from @ Gaondarech726/siteM8@80ca94830347943ea444e8a5af803e0fb7aa57b4 🚀

---
+++ static/js/275.27c1a160.chunk.js
@@ -0,0 +1,37 @@
+"use strict";(self.webpackChunksiteM8=self.webpackChunksiteM8||[]).push([[275],{4275:(e,r,i)=>{i.r(r),i.d(r,{CardWrapper:()=>t,Container:()=>o,default:()=>n});var s=i(8035),a=i(5867),c=i(184);const o=a.ZP.div`
+  margin-top: 50px;
+  width: 928px;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  gap: 16px;
+`,t=a.ZP.div`
+  width: 928px;
+  height: 64px;
+  border-radius: 4px;
+  transition: background-color 0.5s ease, color 0.5s ease;
+  color: white;
+
+  &:hover {
+    background-color: white;
+    color: black;
+  }
+
+  > a {
+    font-size: 18px;
+    font-weight: 600;
+    text-decoration: none;
+    display: block;
+    padding: 20px;
+    color: inherit;
+    transition: color 0.5s ease;
+  }
+  h3 {
+    color: inherit;
+    height: 32px;
+    width: 928px;
+    display: flex;
+    align-items: center;
+  }
+`,n=()=>(0,c.jsxs)(o,{className:"conta",children:[(0,c.jsx)(t,{className:"cardWrapp",children:(0,c.jsx)(s.rU,{to:"runes",children:"\u041f\u0440\u043e \u0440\u0443\u043d\u0438"})}),(0,c.jsx)(t,{className:"cardWrapp",children:(0,c.jsx)(s.rU,{to:"concert",children:"\u041a\u043e\u043d\u0446\u0435\u0440\u0442 M8L8TH"})}),(0,c.jsx)(t,{className:"cardWrapp",children:(0,c.jsx)(s.rU,{to:"sxe",children:"\u0411\u0443\u0434\u044c \u0442\u0432\u0435\u0440\u0435\u0437\u0438\u043c"})})]})}}]);
+//# sourceMappingURL=275.27c1a160.chunk.js.map
